26dd25a96a2d70cd9803eed995e4942b97233940
KeeeeEf <110240392+KeeeeEf@users.noreply.github.com>
2022-12-10 21:01:13 +0800

    index

---
+++ src/footer.js
@@ -0,0 +1,31 @@
+import './App.css'
+
+const Footer = () =>{
+    return(
+        <footer>
+            <div className="footer">
+                <div className="left">
+                    <img src="./logo.png" alt="" />
+                </div>
+                <div className="middle">
+                    <h1>Get In Touch</h1>
+                    <p>Cold Brew Coffee Online (Cebu City)</p>
+                    <p>Mobile: +63xxxxxxxxxx</p>
+                    <p>Cold Brew (Casili, Consolacion)</p>
+                    <p>Mobile: 09xxxxxxxxx</p>
+                    <p>Email us: coldbrewcoffee@gmail.com</p>
+                    <br></br>
+                    <p>69, Dove Street, Casili Consolacion Cebu.</p>
+                </div>
+                <div className="right">
+                    <h1>Follow Us</h1>
+                    <a href="#" class="fa fa-facebook links"></a>
+                    <a href="#" class="fa fa-twitter links"></a>
+                    <a href="#" class="fa fa-instagram links"></a>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/header.js
@@ -0,0 +1,22 @@
+import './App.css'
+
+const Header = () =>{
+    return(
+            <header>
+                <div className='header'>
+                    <div className="left">
+                    <img src="./logo.png" alt=""></img>
+                    <h2>COLD BREW</h2>
+                    </div>
+                    <div className='right'>
+                        <a href='#'>About</a>
+                        <a href='#'>Store</a>
+                        <a href='#'>Cart</a>
+                        <a href='#'>Account</a>
+                    </div>
+                </div>
+            </header>
+    )
+}
+
+export default Header
