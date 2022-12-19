86a1017d81e4c6413a717b08d2f72a26044acb15
KeeeeEf <110240392+KeeeeEf@users.noreply.github.com>
2022-12-19 23:18:42 +0800

    shop page change

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 
+
 import Axios from 'axios';
 import Footer from '../components/footer';
 import Header from '../components/header';
@@ -11,7 +12,12 @@ import '../css/store.css'
 const  Shop = () => {
 
     const [prods, setProds] = useState('');
-
+    const [modal, setModal] = useState([]);
+    const [poptoggle, setpoptoggle] = useState(false); 
+    const getData = (product) => {
+        setModal([product]);
+        setpoptoggle(!poptoggle);
+       }
 
 
     useEffect(()=>{
@@ -28,6 +34,8 @@ const  Shop = () => {
         <div className="ShopBody">
             {prods.map(product =>{
                 return(
+                    <button className='Btn' onClick={()=>getData(product)}>
+                    <div className="">
                     <div className="Products P1" key={product.id}>
                         <img src={product.img} alt="" />
                         <div className="bottom">
@@ -38,8 +46,32 @@ const  Shop = () => {
                             <p>350 ml</p>
                         </div>
                     </div>
+                    </div>
+                    </button>
                 );
             })}
+            {poptoggle&&( <div className="Modal_container">
+                <div className="Decoy" onClick={getData}></div>
+                <div className="Modal_body">
+                <div className="Modal_header">
+                    <button className='Close' onClick={getData}>x</button>
+                </div>
+                 {modal.map((pop)=>{
+                      return(
+                      <div className="Modal" >
+                        <div className="Modal_img">
+                        <img src={pop.img} alt="" />
+                        </div>
+                        <div className="Modal_content">
+                       <p>{[pop.name]}</p>
+                       <p>I'll finish toms.</p>
+                       </div>
+                      </div>
+                      )
+                })}
+            </div>
+            </div>
+            )}
         </div>
         <Footer/>
     </div>
